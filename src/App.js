91e6fdd0cca78bdb9a06f9f44b71b4155055fec8
underhazee <danila.morgun@gmail.com>
2022-08-27 17:30:29 +0200

    create location component and styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import "./scss/app.scss";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import WelcomeNews from "./components/WelcomeNews";
+import Location from "./components/Location";
+import React from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [news, setNews] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `http://127.0.0.1:8000/api/news/?format=json`
+      )
+      .then((res) => {
+        setNews(res.data);
+      });
+  }, []);
+
+  console.log(news);
+
   return (
     <div className="container">
       <Header />
-      <Welcome />
+      <Welcome />      
       <WelcomeNews />
+      <Location />
     </div>
   );
 }
